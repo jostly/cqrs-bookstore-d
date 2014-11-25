8288b6d57b208378bcae45ea95f2d6547b22dbdd
Johan Östling <johan.f.ostling@gmail.com>
2014-11-25 21:44:27 +0100

    Fixed port and CORS settings

--- order-context/source/app.d
+++ order-context/source/app.d
@@ -7,6 +7,15 @@ import cqrslib.bus;
 void accessFilter(HTTPServerRequest req, HTTPServerResponse res)
 {
     res.headers["Access-Control-Allow-Origin"] = "*";
+    res.headers["Access-Control-Allow-Methods"] = "GET,PUT,POST,DELETE,OPTIONS";
+    res.headers["Access-Control-Allow-Headers"] = "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin";
+    res.headers["Access-Control-Allow-Credentials"] = "true"; 
+}
+
+void sendOptions(HTTPServerRequest req, HTTPServerResponse res)
+{
+    accessFilter(req, res);
+    res.writeBody("");
 }
 
 shared static this()
@@ -30,12 +39,13 @@ shared static this()
 
 	auto router = new URLRouter;
 	router.any("*", &accessFilter);
+	router.match(HTTPMethod.OPTIONS, "*", &sendOptions);
 	
 	router.registerRestInterface(new QueryResource(queryService, domainEventStore), "/service/query");
 	router.registerRestInterface(new OrderResource(commandBus), "/service/order-requests");
 
 	auto settings = new HTTPServerSettings;
-	settings.port = 8080;
+	settings.port = 8070;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
 	settings.accessLogToConsole = true; // Log all access to the console
 	listenHTTP(settings, router);
